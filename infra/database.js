5cced1a6758b1da338b6d6cee5b38caa387337c1
Roger Dahian <rogerwop@hotmail.com>
2026-06-04 13:31:58 +0000

    finalizar endpoint status

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,8 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
-  const client = new Client({
+  let client;
+  client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
@@ -9,11 +10,16 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
-}
 
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
 export default {
   query: query,
 }
